1c8859aaa54d4a41ffd6d37a31d595b0a849fcad
bcl <bcl9683@louisiana.edu>
2019-04-04 13:00:35 -0500

    427 Presentation Demo

---
+++ 427 - Presentation project/Assets/Menu.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameWithUI");
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public GameObject pausemenu;
+    public bool ispaused;
+
+
+    public void ResumeGame()
+    {
+        ispaused = false;
+        pausemenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (ispaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                ispaused = true;
+                pausemenu.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+    }
+
+
+    public GameObject ResolutionPanel;
+    public void OpenOptions()
+    {
+        ResolutionPanel.SetActive(true);
+    }
+    public int width;
+    public int height;
+    public void SetWidth(int newwidth){
+        width = newwidth;
+    }
+    public void SetHeight(int newheight)
+    {
+        height = newheight;
+    }
+    public void SetRes()
+    {
+        Screen.SetResolution(width, height, false);
+    }
+
+
+    
+}
